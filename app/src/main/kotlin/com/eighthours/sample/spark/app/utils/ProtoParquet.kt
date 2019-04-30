6cdfe08c494b0158014a77f3633de0fb20da5af0
kazumoto <motch0214@gmail.com>
2019-05-01 01:28:00 +0900

    refactor: Modify parquet writer buidling

--- app/src/main/kotlin/com/eighthours/sample/spark/app/utils/ProtoParquet.kt
+++ app/src/main/kotlin/com/eighthours/sample/spark/app/utils/ProtoParquet.kt
@@ -1,15 +1,26 @@
 package com.eighthours.sample.spark.app.utils
 
 import com.google.protobuf.Message
-import org.apache.hadoop.fs.Path
+import org.apache.hadoop.conf.Configuration
 import org.apache.parquet.hadoop.ParquetWriter
-import org.apache.parquet.proto.ProtoParquetWriter
+import org.apache.parquet.hadoop.api.WriteSupport
+import org.apache.parquet.proto.ProtoWriteSupport
 import java.net.URI
 import kotlin.reflect.KClass
 
 internal object ProtoParquet {
 
-    fun <M : Message> openWriter(uri: URI, messageClass: KClass<M>, block: (ParquetWriter<M>) -> Unit) {
-        ProtoParquetWriter<M>(Path(uri), messageClass.java).use(block)
+    fun <M : Message> openWriter(uri: URI, messageType: KClass<M>, block: (ParquetWriter<M>) -> Unit) {
+        WriterBuilder(uri, messageType).build().use(block)
+    }
+}
+
+private class WriterBuilder<M : Message>(uri: URI, val messageType: KClass<M>)
+    : ParquetWriter.Builder<M, WriterBuilder<M>>(org.apache.hadoop.fs.Path(uri)) {
+
+    override fun self(): WriterBuilder<M> = this
+
+    override fun getWriteSupport(conf: Configuration?): WriteSupport<M> {
+        return ProtoWriteSupport(messageType.java)
     }
 }
